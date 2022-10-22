609f24d07f60b492d99b91e756d9d1277c7f71e5
Yozora <yozora391@outlook.com>
2022-10-22 12:23:43 +0300

    06_fix

--- pybuild.bat
+++ pybuild.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 :start
 python nml_patcher.py -f "JPmetro.pnml" -o "JPmetro.nml" -b 0 -v 1
-nmlc JPmetro.nml -o D:\DocumentsWin\OpenTTD\newgrf\JPmetro.grf
+nmlc JPmetro.nml -o D:\DocumentsWin\OpenTTD\newgrf\JPmetro.grf -o JPmetro.grf
 PAUSE
 goto start
