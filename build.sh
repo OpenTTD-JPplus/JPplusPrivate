73c475f3f6be2d6c086aeb2650c5697ad921b5ab
Jeremy Gao <145064583+WenSimEHRP@users.noreply.github.com>
2024-05-24 13:31:05 -0700

    Fix: logos not dynamic

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # use gcc to preprocess the file
 # use nmlc to compile the file
-gcc -E -x c -o JPprivate.nml JPprivate.pnml && \
-nmlc JPprivate.nml -o JPprivate.grf --nml="JPprivate_parsed.nml" && \
-echo "done"
+gcc -E -x c -o JPprivate.nml JPprivate.pnml &&
+    echo "done" &&
+    nmlc JPprivate.nml -o JPprivate.grf --nml="JPprivate_parsed.nml" &&
+    echo "done"
