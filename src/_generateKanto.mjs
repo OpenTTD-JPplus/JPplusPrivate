8a03756d153062c94f18a65d32f9b9336503dc16
Yozora <yozora391@yandex.com>
2025-12-07 21:56:05 +0300

    add: toei 5000~5500

--- src/_generateKanto.mjs
+++ src/_generateKanto.mjs
@@ -1,4 +1,6 @@
-import { parseCompany } from "../../api/utils/parseData.mjs";
+import { parseCompany } from "./api/utils/parseData.mjs";
+
+parseCompany("src/metro/Tokyo/asakusa");
 
 parseCompany("src/private/Kanto/TWR");
 parseCompany("src/private/Kanto/tsukuba");
