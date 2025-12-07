2885da80183a210f96142a4b356a10576c8564d1
Yozora <yozora391@yandex.com>
2025-12-08 00:07:31 +0300

    add: fukuoka

--- src/_generateKanto.mjs
+++ src/_generateKanto.mjs
@@ -2,6 +2,8 @@ import { parseCompany } from "./api/utils/parseData.mjs";
 
 parseCompany("src/metro/Tokyo/asakusa");
 
+parseCompany("src/metro/Fukuoka");
+
 parseCompany("src/private/Kanto/TWR");
 parseCompany("src/private/Kanto/tsukuba");
 parseCompany("src/private/Kanto/tokyu");
