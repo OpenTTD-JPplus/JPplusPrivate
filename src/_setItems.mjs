509c2784283d8727006f2b831b4ef753aae1bc57
Yozora <yozora391@gmail.com>
2025-12-11 01:18:23 +0300

    add: nagoya metro

---
+++ src/_setItems.mjs
@@ -0,0 +1,11 @@
+import { parseCompany } from "./api/utils/parseData.mjs";
+
+parseCompany("src/metro/Tokyo/asakusa");
+
+parseCompany("src/metro/Kyoto");
+parseCompany("src/metro/Nagoya");
+parseCompany("src/metro/Fukuoka");
+
+parseCompany("src/private/Kanto/TWR");
+parseCompany("src/private/Kanto/tsukuba");
+parseCompany("src/private/Kanto/tokyu");
