4471112463817125a407cac5bc598cbf08c6303d
Yozora <yozora391@gmail.com>
2026-08-08 01:04:33 +0300

    add: chibaNT9200/9800

--- src/_setItems.mjs
+++ src/_setItems.mjs
@@ -20,4 +20,5 @@ parseCompany("src/private/Kanto/odakyu");
 parseCompany("src/private/Kanto/sotetsu");
 parseCompany("src/private/Kanto/keio");
 parseCompany("src/private/Kanto/keisei");
+parseCompany("src/private/Kanto/chibaNewTown");
 parseCompany("src/private/Kanto/shibayama");
